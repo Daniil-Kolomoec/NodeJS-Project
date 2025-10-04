ee911956374ba9df0c34edbf49b3bce15ebce35e
Daniil Kolomoec <denter257@gmail.com>
2025-10-04 15:03:27 +0200

    Implementing /posts with skip and take, adding route /posts/:id

--- server.js
+++ server.js
@@ -1,48 +1,64 @@
 const express = require('express')
-const moment = require('moment');
 
 const app = express()
 
 const path = require('path')
+
 const fs = require('fs')
 
+const HOST = "localhost"
 const PORT = 8001
 
-const postsPath = path.join(__dirname, "posts.json")
 
+const postsPath = path.join(__dirname, "posts.json")
 const posts = JSON.parse(fs.readFileSync(postsPath, 'UTF-8'))
 
-// function getCurrentDay() {
-//   console.log(moment().format('dddd'))
-// }
 
-// function getCurrentMonth() {
-//   console.log(moment().format('MMMM'))
-// }
-
-
-// function getCurrentYear()
-//   console.log(moment().format('YYYY'))
-// }
-
-
-function getDate() {
-  return moment().format('YYYY/DD/MM HH:mm:ss')
-}
-
-app.get('/timestamp', (req, res) => {
-  res.json({ timestamp: getDate() })
+app.get('/', (req, res) => {
+  res.json('Server is working')
 })
+
 
 app.get('/posts', (req, res) => {
-    res.status(200).json(posts)
+  const skip = req.query.skip
+  const take = req.query.take
+
+  if (skip && isNaN(+skip)) {
+    res.status(400).json("Skip must be an Integer")
+    return
+  }
+
+  if (take && isNaN(+take)) {
+    res.status(400).json("Take must be an Integer")
+    return
+  }
+
+  const skipNum = +skip || 0
+  const takeNum = +take || posts.length
+
+  const result = posts.slice(skipNum, skipNum + takeNum)
+  res.status(200).json(result)
 })
 
-// getCurrentDay()
-// getCurrentMonth()
-// getCurrentYear()
-// getDate()
 
-app.listen(PORT, () => {
+app.get('/posts/:id', (req, res) => {
+  const id = +req.params.id
+
+  if (isNaN(id)) {
+    res.status(400).json("ID must be a number")
+    return
+  }
+
+  const post = posts.find(p => p.id === id)
+  if (!post) {
+    res.status(404).json("Post not found")
+    return
+  }
+
+  res.status(200).json(post)
+})
+
+app.listen(PORT, HOST, () => {
   console.log("Server is running on http://localhost:8001")
 })
+
